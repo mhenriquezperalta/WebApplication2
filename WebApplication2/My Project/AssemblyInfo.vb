cda22f972300818574587f34ca0df4d783504fbb
manuel henriquez <mhenriquezperalta@gmail.com>
2025-03-23 21:03:18 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("WebApplication2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication2")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("b622a94b-29de-4642-8cc6-54db1d4941ed")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ WebApplication2/WebService1.asmx.vb
@@ -0,0 +1,76 @@
+﻿Imports System.ComponentModel
+Imports System.Web.Script.Serialization
+Imports System.Web.Script.Services
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+
+' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
+<System.Web.Script.Services.ScriptService()>
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
+Public Class WebService1
+    Inherits System.Web.Services.WebService
+
+    <WebMethod()>
+    Public Function HelloWorld() As String
+        Return "Hola a todos"
+    End Function
+
+    <WebMethod()>
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Sub ObtenerDatos()
+        ' Crear un objeto de ejemplo
+        Dim datos As New With {
+            .Nombre = "Juan",
+            .Edad = 30,
+            .Ciudad = "Madrid"
+        }
+
+        ' Serializar el objeto a JSON
+        Dim jsonSerializer As New JavaScriptSerializer()
+        Dim json As String = jsonSerializer.Serialize(datos)
+
+        ' Establecer el tipo de contenido a JSON
+        HttpContext.Current.Response.ContentType = "application/json"
+        HttpContext.Current.Response.Write(json)
+        HttpContext.Current.ApplicationInstance.CompleteRequest()
+
+    End Sub
+
+    <WebMethod()>
+    <ScriptMethod(UseHttpGet:=False, ResponseFormat:=ResponseFormat.Json)>
+    Public Sub IngresarDatos(ByVal persona As Person)
+        Try
+
+            ' Crear un objeto de ejemplo
+            Dim datos As New With {
+                .Nombre = persona.Nombre,
+                .Edad = persona.Edad,
+                .Ciudad = persona.Ciudad
+            }
+
+            ' Serializar el objeto a JSON
+            Dim jsonSerializer As New JavaScriptSerializer()
+            Dim json As String = jsonSerializer.Serialize(datos)
+
+            ' Establecer el tipo de contenido a JSON
+            HttpContext.Current.Response.ContentType = "application/json"
+            HttpContext.Current.Response.Write(json)
+            'HttpContext.Current.Response.End()
+            ' Finalizar correctamente sin abortar el hilo
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+            Console.ReadLine()
+        End Try
+
+    End Sub
+
+End Class
+
+Public Class Person
+    Public Property Nombre As String
+    Public Property Edad As Integer
+    Public Property Ciudad As String
+End Class
